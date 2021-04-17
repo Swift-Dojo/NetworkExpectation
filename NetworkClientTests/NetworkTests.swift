65196a98dae16bca2400468af08ad4f6a5d8616a
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2021-04-16 19:15:41 -0500

    Use expectations for asserting delivered data

--- NetworkClientTests/NetworkTests.swift
+++ NetworkClientTests/NetworkTests.swift
@@ -5,13 +5,30 @@ import XCTest
 final class NetworkTests: XCTestCase {
 
     func test_request() {
-        let client = NetworkClientStub()
-        let sut = Network(client: client)        
+        let anyData = Data()
+        let anyURL = URL(string: "https://google.com")!
+        let client = NetworkClientStub(completesWith: anyData)
+        let sut = Network(client: client)
+        let exp = expectation(description: "Network client expectation")
+        
+        sut.request(url: anyURL) { data in
+            XCTAssertNotNil(data)
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
     }
 }
 
 private class NetworkClientStub: NetworkClient {
+    
+    private let data: Data
+    
+    init(completesWith data: Data) {
+        self.data = data
+    }
+    
     func get(for url: URL, completion: @escaping (Data) -> Void) {
-        completion(Data())
+        completion(data)
     }
 }
